d342622ace2714707d2bfb1a72d67aa13cbe9735
Ife <sunmolaife@gmail.com>
2022-08-16 01:51:27 -0600

    Updated test data

--- src/main/java/com/example/sellthatthing/SellThatThingApplication.java
+++ src/main/java/com/example/sellthatthing/SellThatThingApplication.java
@@ -1,7 +1,11 @@
 package com.example.sellthatthing;
 
+import com.example.sellthatthing.DTOs.NewAccountRequest;
+import com.example.sellthatthing.DTOs.NewPostRequest;
+import com.example.sellthatthing.models.Account;
 import com.example.sellthatthing.models.Category;
 import com.example.sellthatthing.models.Post;
+import com.example.sellthatthing.services.AccountService;
 import com.example.sellthatthing.services.CategoryService;
 import com.example.sellthatthing.services.PostService;
 import lombok.AllArgsConstructor;
@@ -16,6 +20,7 @@ import java.time.LocalDateTime;
 public class SellThatThingApplication implements CommandLineRunner {
     private final CategoryService categoryService;
     private final PostService postService;
+    private final AccountService accountService;
 
     public static void main(String[] args) {
         SpringApplication.run(SellThatThingApplication.class, args);
@@ -26,32 +31,50 @@ public class SellThatThingApplication implements CommandLineRunner {
         Category category1 = new Category();
         category1.setCategoryName("Books");
         category1.setDateCreated(LocalDateTime.of(2021, 11, 24, 12, 55));
-        categoryService.saveNewCategory(category1);
 
         Category category2 = new Category();
         category2.setCategoryName("Vehicles");
         category2.setDateCreated(LocalDateTime.of(2022, 1, 11, 20, 3));
-        categoryService.saveNewCategory(category2);
 
         Category category3 = new Category();
         category3.setCategoryName("Computers");
         category3.setDateCreated(LocalDateTime.of(2019, 5, 21, 19, 22));
-        categoryService.saveNewCategory(category3);
 
         Post post1 = new Post();
         post1.setTitle("Title of Post 1");
         post1.setBody("Body of Post 1");
+        post1.setPostCategory(category1);
+        post1.setPosterAccount(new Account(
+                "Ife",
+                "Sunmola",
+                "sunmolaife@gmail.com",
+                "password"
+        ));
 
         Post post2 = new Post();
         post2.setTitle("Title of Post 2");
         post2.setBody("Body of Post 2");
+        post2.setPostCategory(category2);
+        post2.setPosterAccount(new Account(
+                "jane",
+                "foster",
+                "janefoster@gmail.com",
+                "password"
+        ));
 
         Post post3 = new Post();
         post3.setTitle("Title of Post 3");
         post3.setBody("Body of Post 3");
+        post3.setPostCategory(category3);
+        post3.setPosterAccount(new Account(
+                "thor",
+                "ragnarok",
+                "thorragnarok@gmail.com",
+                "password"
+        ));
 
-//        postService.savePost(post1);
-//        postService.savePost(post2);
-//        postService.savePost(post3);
+        postService.savePost(post1);
+        postService.savePost(post2);
+        postService.savePost(post3);
     }
 }
