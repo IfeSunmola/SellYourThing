96c8d575ea8fae0396e33971ef1500c1031b68e7
Ife <sunmolaife@gmail.com>
2022-08-16 02:15:38 -0600

    Added NoArgsConst

--- src/main/java/com/example/sellthatthing/DTOs/NewPostRequest.java
+++ src/main/java/com/example/sellthatthing/DTOs/NewPostRequest.java
@@ -2,9 +2,11 @@ package com.example.sellthatthing.DTOs;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class NewPostRequest {
     private String title;
     private String body;
