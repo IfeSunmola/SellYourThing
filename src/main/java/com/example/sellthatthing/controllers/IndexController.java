b39bd33460c453dc85c4fee418db21257d04f0dc
Ife <sunmolaife@gmail.com>
2022-08-17 19:54:25 -0600

    added posts findAll model attribute

--- src/main/java/com/example/sellthatthing/controllers/IndexController.java
+++ src/main/java/com/example/sellthatthing/controllers/IndexController.java
@@ -1,6 +1,7 @@
 package com.example.sellthatthing.controllers;
 
 import com.example.sellthatthing.services.CategoryService;
+import com.example.sellthatthing.services.PostService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -10,10 +11,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 @AllArgsConstructor
 public class IndexController {
     private final CategoryService categoryService;
+    private final PostService postService;
 
     @GetMapping
     public String showIndexPage(Model model){
         model.addAttribute("categories", categoryService.findAll());
+        model.addAttribute("posts", postService.findAll());
         return "index";
     }
 }
