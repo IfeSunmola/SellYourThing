da3713b2f0c6220614735678b596cb071e2d1407
Ife <sunmolaife@gmail.com>
2022-08-20 01:18:10 -0600

    Added model attributes

--- src/main/java/com/example/sellthatthing/controllers/RegisterController.java
+++ src/main/java/com/example/sellthatthing/controllers/RegisterController.java
@@ -1,14 +1,28 @@
 package com.example.sellthatthing.controllers;
 
+import com.example.sellthatthing.DTOs.NewAccountRequest;
+import com.example.sellthatthing.services.AccountService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @AllArgsConstructor
 public class RegisterController {
+    private final AccountService accountService;
+
     @GetMapping("/register")
-    public String loadRegisterPage(){
+    public String loadRegisterPage(Model model){
+        model.addAttribute("registrationDto", new NewAccountRequest());
         return "register";
+    }
+
+    @PostMapping("/registration")
+    public String processRegisterForm(@ModelAttribute NewAccountRequest newAccountRequest){
+        accountService.createAccount(newAccountRequest);
+        return "redirect:/login"; // redirect user to login page after registration
     }
 }
