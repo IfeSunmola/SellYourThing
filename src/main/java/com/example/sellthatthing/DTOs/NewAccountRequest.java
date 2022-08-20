e800ecfa6b9c32ea876ab2deb1eceab63ee54459
Ife <sunmolaife@gmail.com>
2022-08-19 23:28:04 -0600

    Added no args constructor

--- src/main/java/com/example/sellthatthing/DTOs/NewAccountRequest.java
+++ src/main/java/com/example/sellthatthing/DTOs/NewAccountRequest.java
@@ -2,9 +2,11 @@ package com.example.sellthatthing.DTOs;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class NewAccountRequest {
     private String firstName;
     private String lastName;
