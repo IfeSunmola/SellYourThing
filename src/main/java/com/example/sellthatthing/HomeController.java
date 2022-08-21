09be2064528cb19a9a8f417142006d53f226d005
Ife <sunmolaife@gmail.com>
2022-08-21 00:50:17 -0600

    temp

--- src/main/java/com/example/sellthatthing/HomeController.java
+++ src/main/java/com/example/sellthatthing/HomeController.java
@@ -2,6 +2,9 @@ package com.example.sellthatthing;
 
 import com.example.sellthatthing.Post.PostService;
 import lombok.AllArgsConstructor;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,8 +15,17 @@ public class HomeController {
     private final PostService postService;
 
     @GetMapping
-    public String showIndexPage(Model model) {
+    public String showIndexPage(final Model model) {
         model.addAttribute("posts", postService.findAll());
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (!(auth instanceof AnonymousAuthenticationToken)) { // user is authenticated
+            System.out.print("");
+        }
         return "index";
+    }
+
+    @GetMapping("/css/style.css")
+    public String temp() {
+        return "redirect:/";
     }
 }
