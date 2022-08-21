bec44b07e22ecf153b94cc4adca852c1ab39eb07
Ife <sunmolaife@gmail.com>
2022-08-21 02:05:52 -0600

    Added profileLink model for user nav dropdown menu

--- src/main/java/com/example/sellthatthing/HomeController.java
+++ src/main/java/com/example/sellthatthing/HomeController.java
@@ -1,5 +1,6 @@
 package com.example.sellthatthing;
 
+import com.example.sellthatthing.Account.AccountService;
 import com.example.sellthatthing.Post.PostService;
 import lombok.AllArgsConstructor;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
@@ -13,13 +14,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 @AllArgsConstructor
 public class HomeController {
     private final PostService postService;
+    private final AccountService accountService;
 
     @GetMapping
     public String showIndexPage(final Model model) {
         model.addAttribute("posts", postService.findAll());
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         if (!(auth instanceof AnonymousAuthenticationToken)) { // user is authenticated
-            System.out.print("");
+            // for some reason, I can't construct the link in thymeleaf with th:href="|@{/users/${#authentication.principal.accountId}}|",
+            // so I have to do it here
+            String email = auth.getName();
+            String profileLink = "/users/" + accountService.findByEmail(email).getAccountId();
+            model.addAttribute("profileLink", profileLink);
         }
         return "index";
     }
