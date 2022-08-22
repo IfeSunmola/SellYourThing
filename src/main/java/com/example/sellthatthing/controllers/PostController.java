6d6b04e6a58977d12f8ff152870db2b34dda1bf3
Ife <sunmolaife@gmail.com>
2022-08-22 16:20:48 -0600

    Updated processNewPostForm

--- src/main/java/com/example/sellthatthing/controllers/PostController.java
+++ src/main/java/com/example/sellthatthing/controllers/PostController.java
@@ -1,10 +1,15 @@
 package com.example.sellthatthing.controllers;
 
+import com.example.sellthatthing.models.Account;
+import com.example.sellthatthing.services.AccountService;
 import com.example.sellthatthing.services.CategoryService;
 import com.example.sellthatthing.datatransferobjects.NewPostRequest;
 import com.example.sellthatthing.models.Post;
 import com.example.sellthatthing.services.PostService;
 import lombok.AllArgsConstructor;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -14,6 +19,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/posts")
 public class PostController {
     private final PostService postService;
+    private final AccountService accountService;
     private final CategoryService categoryService;
 
     @GetMapping("/{postId}")
@@ -33,7 +39,12 @@ public class PostController {
 
     @PostMapping("/create-new")
     public String processNewPostForm(@ModelAttribute NewPostRequest newPostRequest) {
-        postService.createNewPost(newPostRequest);
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if ((auth instanceof AnonymousAuthenticationToken)) {
+            // user is not authenticated, shouldn't happen since only authenticated users can view the page
+            return "redirect:/login";
+        }
+        postService.createNewPost(newPostRequest, auth);
         return "redirect:/all";
     }
 }
