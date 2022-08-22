92aa34a77c90466d703dff4b83394cc0f0118b0a
Ife <sunmolaife@gmail.com>
2022-08-22 14:31:41 -0600

    Made thor admin

--- src/main/java/com/example/sellthatthing/SellThatThingApplication.java
+++ src/main/java/com/example/sellthatthing/SellThatThingApplication.java
@@ -99,7 +99,7 @@ public class SellThatThingApplication implements CommandLineRunner {
         post3.setPosterAccount(new Account(
                 "thor",
                 "ragnarok",
-                "thorragnarok@gmail.com",
+                "thorragnarok@company.ca",
                 LocalDate.of(1758, 5, 17),
                 "User",
                 "$2a$10$eiJzFSAUeDX2Xo29rPAs0.1ho4i0LyjItvbczuliI8mrSF6sBEWBS"
