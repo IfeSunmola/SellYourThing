c07bb28dbcdf415ca8a527115cce7f84e3bf09f5
Ife <sunmolaife@gmail.com>
2022-08-22 19:56:53 -0600

    Added @Transactional(readOnly = true)

--- src/main/java/com/example/sellthatthing/repositories/PostRepository.java
+++ src/main/java/com/example/sellthatthing/repositories/PostRepository.java
@@ -5,10 +5,12 @@ import com.example.sellthatthing.models.Category;
 import com.example.sellthatthing.models.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Repository
+@Transactional(readOnly = true)
 public interface PostRepository extends JpaRepository<Post, Long> {
     List<Post> findPostsByPosterAccount(Account account);
     List<Post> findByPostCategory(Category category);
