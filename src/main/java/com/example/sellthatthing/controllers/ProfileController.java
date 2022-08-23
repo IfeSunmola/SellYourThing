3a76f18a522384039cf52f8ce236fcbdae6aa14d
Ife <sunmolaife@gmail.com>
2022-08-23 00:41:01 -0600

    Only account owner can see delete buttons, and other things

--- src/main/java/com/example/sellthatthing/controllers/ProfileController.java
+++ src/main/java/com/example/sellthatthing/controllers/ProfileController.java
@@ -2,21 +2,32 @@ package com.example.sellthatthing.controllers;
 
 import com.example.sellthatthing.models.Account;
 import com.example.sellthatthing.services.AccountService;
+import com.example.sellthatthing.services.PostService;
 import lombok.AllArgsConstructor;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 @AllArgsConstructor
-@RequestMapping("/profile")
+@RequestMapping("/users")
 public class ProfileController {
     private final AccountService accountService;
+    private final PostService postService;
 
     @GetMapping("/{accountId}")
     public String loadProfilePage(@PathVariable Long accountId, Model model) {
-        Account account = accountService.findByAccountId(accountId);
-        model.addAttribute("accountInfo", account);
+        Account currentAccount = accountService.findByAccountId(accountId);
+        model.addAttribute("currentAccount", currentAccount);
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (!(auth instanceof AnonymousAuthenticationToken)) { // user is authenticated
+            Account authAccount = (Account) auth.getPrincipal();
+            model.addAttribute("authAccount", authAccount);
+            model.addAttribute("isSameUser", authAccount.getAccountId().equals(currentAccount.getAccountId()));
+        }
         return "profile";
     }
 
@@ -34,7 +45,7 @@ public class ProfileController {
 
     @GetMapping("/{accountId}/posts")
     public String getAllUserPosts(@PathVariable Long accountId, Model model) {
-        model.addAttribute("allUserPosts", accountService.findPostsByAccountId(accountId));
-        return "view-post";
+        model.addAttribute("userPosts", postService.findAllAccountPosts(accountId));
+        return "view-user-posts";
     }
 }
