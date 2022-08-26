2a039b26837930ac4c2eeead73ae59088d239a4f
Ife <sunmolaife@gmail.com>
2022-08-25 21:35:30 -0600

    Added constrctor for NewRequestDto

--- src/main/java/com/example/sellthatthing/models/Account.java
+++ src/main/java/com/example/sellthatthing/models/Account.java
@@ -1,15 +1,18 @@
 package com.example.sellthatthing.models;
 
+import com.example.sellthatthing.datatransferobjects.NewAccountRequest;
 import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import javax.persistence.*;
 import java.time.LocalDate;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
+import java.util.Locale;
 
 @Entity
 @Getter
@@ -41,5 +44,14 @@ public class Account implements UserDetails {
     @Override
     public String getUsername() {
         return this.email;
+    }
+
+    public Account(final NewAccountRequest newAccountRequest, final BCryptPasswordEncoder passwordEncoder) {
+        firstName = newAccountRequest.getFirstName();
+        lastName = newAccountRequest.getLastName();
+        email = newAccountRequest.getEmail();
+        dateOfBirth = newAccountRequest.getDateOfBirth();
+        role = newAccountRequest.getEmail().toLowerCase(Locale.ROOT).contains("@company.ca") ? "ADMIN" : "USER";
+        password = passwordEncoder.encode(newAccountRequest.getPassword());
     }
 }
