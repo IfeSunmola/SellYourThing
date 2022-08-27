5f010daf1a05f827d8f315c848091dc4d6b2f429
Ife <sunmolaife@gmail.com>
2022-08-27 13:27:05 -0600

    Request Param is no longer needed to show error message

--- src/main/java/com/example/sellthatthing/controllers/LoginController.java
+++ src/main/java/com/example/sellthatthing/controllers/LoginController.java
@@ -1,9 +1,16 @@
 package com.example.sellthatthing.controllers;
 
 import lombok.AllArgsConstructor;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.WebAttributes;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 @Controller
 @AllArgsConstructor
@@ -12,5 +19,20 @@ public class LoginController {
     @GetMapping
     public String loadLoginPage() {
         return "login";
+    }
+
+    @GetMapping("/login-error")
+    public String login(HttpServletRequest request, Model model, RedirectAttributes redirectAttributes) {
+        HttpSession session = request.getSession(false);
+        String errorMessage = null;
+        if (session != null) {
+            AuthenticationException ex = (AuthenticationException) session.getAttribute(WebAttributes.AUTHENTICATION_EXCEPTION);
+            if (ex != null) {
+                errorMessage = ex.getMessage();
+            }
+        }
+//        model.addAttribute("errorMessage", errorMessage);
+        redirectAttributes.addFlashAttribute("errorMessage", true);
+        return "redirect:/login";
     }
 }
