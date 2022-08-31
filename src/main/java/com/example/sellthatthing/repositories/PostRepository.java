d2ac21230a5ac79225f68f3326c69e13ed6898fe
Ife <sunmolaife@gmail.com>
2022-08-30 23:36:39 -0600

    Done with sorting

--- src/main/java/com/example/sellthatthing/repositories/PostRepository.java
+++ src/main/java/com/example/sellthatthing/repositories/PostRepository.java
@@ -3,7 +3,10 @@ package com.example.sellthatthing.repositories;
 import com.example.sellthatthing.models.Account;
 import com.example.sellthatthing.models.Category;
 import com.example.sellthatthing.models.Post;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,4 +18,26 @@ public interface PostRepository extends JpaRepository<Post, Long> {
     List<Post> findPostsByPosterAccount(Account account);
 
     List<Post> findByPostCategory(Category category);
+
+    @Query("""
+                SELECT p from Post p
+                WHERE p.postCity.cityName LIKE %:cityName%
+                AND p.postCategory.categoryName LIKE %:categoryName%
+                AND upper(p.body)  LIKE :searchText%
+            """)
+    List<Post> findAllWithDate(@Param("cityName") String cityName,
+                               @Param("categoryName") String categoryName,
+                               @Param("searchText") String searchText,
+                               Sort sort);
+
+    @Query("""
+                SELECT p from Post p
+                WHERE p.postCity.cityName LIKE %:cityName%
+                AND p.postCategory.categoryName LIKE %:categoryName%
+                AND upper(p.body)  LIKE :searchText%
+            """)
+    List<Post> findAllWithPrice(@Param("cityName") String cityName,
+                                @Param("categoryName") String categoryName,
+                                @Param("searchText") String searchText,
+                                Sort sort);
 }
