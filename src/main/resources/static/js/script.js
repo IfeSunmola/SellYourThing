d7b3ee1788626858c185b67d02706d90bc07d09e
Ife <sunmolaife@gmail.com>
2022-09-01 13:20:51 -0600

    Added function to open disable account modal

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -18,3 +18,28 @@ function enableDeleteButton(email) {
     const confirmEmail = document.getElementById("confirmEmail");
     deleteAccountButton.disabled = confirmEmail.value !== email;
 }
+
+function openModalAndPreFill() {
+    $('document').ready(function () {
+        $('.table .temp').on('click', function (event) {
+            event.preventDefault();
+
+            const href = $(this).attr('href');
+            $.get(href, function (account) {
+                //select accountId, change the value to the data gotten from the get request
+                //name input attribute is used to do the mapping. Name attribute must be the same as the name in the result set
+                $('#accountId').val(account.accountId);
+                $('#role').val(account.role);
+                $('#firstName').val(account.firstName);
+                $('#lastName').val(account.lastName);
+                $('#email').val(account.email);
+                $('#dateOfBirth').val(account.dateOfBirth);
+                $('#active').val(account.active);
+
+            });
+            $('#disableAccountModal').modal('show')
+        })
+    })
+}
+
+openModalAndPreFill()
