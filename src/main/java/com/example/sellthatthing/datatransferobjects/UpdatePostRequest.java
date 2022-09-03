b05f6ba12e0f6f6b5c3c08a06f11d747a5d497a8
Ife <sunmolaife@gmail.com>
2022-09-03 15:18:15 -0600

    Changed category id to be a Long

--- src/main/java/com/example/sellthatthing/datatransferobjects/UpdatePostRequest.java
+++ src/main/java/com/example/sellthatthing/datatransferobjects/UpdatePostRequest.java
@@ -10,7 +10,7 @@ import java.math.BigDecimal;
 public class UpdatePostRequest {
     private final String title;
     private final String body;
-    private final String categoryName;
+    private final Long categoryId;
     private final BigDecimal price;
     private final String imageUrl;
     private final String city;
