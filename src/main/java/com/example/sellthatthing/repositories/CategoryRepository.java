90d3c754f57b1891a8cb2d19ba06f3c969811a36
Ife <sunmolaife@gmail.com>
2022-09-03 15:42:46 -0600

    added updateName

--- src/main/java/com/example/sellthatthing/repositories/CategoryRepository.java
+++ src/main/java/com/example/sellthatthing/repositories/CategoryRepository.java
@@ -2,6 +2,8 @@ package com.example.sellthatthing.repositories;
 
 import com.example.sellthatthing.models.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,4 +17,9 @@ public interface CategoryRepository extends JpaRepository<Category, Long> {
     void deleteByCategoryName(String categoryName);
 
     Optional<Category> findByCategoryName(String categoryName);
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE Category c SET c.categoryName = ?2 WHERE c.categoryName = ?1")
+    void updateName(String oldName, String newName);
 }
