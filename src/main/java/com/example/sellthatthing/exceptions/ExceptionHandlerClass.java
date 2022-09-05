c7dac912d787eca0de4374506b74ee99ef3a9859
Ife <sunmolaife@gmail.com>
2022-09-05 00:28:09 -0600

    minor changes

--- src/main/java/com/example/sellthatthing/exceptions/ExceptionHandlerClass.java
+++ src/main/java/com/example/sellthatthing/exceptions/ExceptionHandlerClass.java
@@ -2,15 +2,18 @@ package com.example.sellthatthing.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.HashMap;
 
-@RestControllerAdvice
-/*
- * This class handles any exception thrown from the CategoryController class
- * */
+/**
+ * @author Ife Sunmola
+ * <p>
+ * This class handles any exception thrown.
+ */
+
+@ControllerAdvice
 public class ExceptionHandlerClass {
     @ExceptionHandler
     public ResponseEntity<HashMap<String, String>> resourceAlreadyExists(ResourceAlreadyExistsException ex) {
@@ -19,11 +22,12 @@ public class ExceptionHandlerClass {
         return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
     }
 
-    @ExceptionHandler
-    public ResponseEntity<HashMap<String, String>> resourceNotFound(ResourceNotFoundException ex) {
-        HashMap<String, String> errorMessage = new HashMap<>(1);
-        errorMessage.put("Error", ex.getMessage());
-        return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
+    /**
+     * Something was not found. Return 404 page
+     */
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public String resourceNotFound() {
+        return "error-pages/404-error";
     }
 
     @ExceptionHandler
