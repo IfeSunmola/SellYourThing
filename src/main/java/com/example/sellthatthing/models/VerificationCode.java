d78e96f0c26fb670cb6ba76acb89d60592ba734e
Ife <sunmolaife@gmail.com>
2022-09-04 21:10:50 -0600

    changed code expiry date to 24 hours

--- src/main/java/com/example/sellthatthing/models/VerificationCode.java
+++ src/main/java/com/example/sellthatthing/models/VerificationCode.java
@@ -17,10 +17,10 @@ public class VerificationCode {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY) private Long codeId;
     @NonNull private final String code = UUID.randomUUID().toString().toUpperCase(Locale.ROOT).substring(0, 6);
     @NonNull private final LocalDateTime createdAt = LocalDateTime.now();
-    @NonNull private final LocalDateTime expiresAt = LocalDateTime.now().plusMinutes(30);
+    @NonNull private final LocalDateTime expiresAt = LocalDateTime.now().plusHours(24);
     private LocalDateTime confirmedAt;
 
-    @ManyToOne
+    @ManyToOne(cascade = CascadeType.PERSIST)
     @JoinColumn(nullable = false, name = "accountId")
     @NonNull private Account account;
 }
