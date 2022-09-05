624fd58453c5a0d85e8b767bc52ac18b8bbb0352
Ife <sunmolaife@gmail.com>
2022-09-05 00:49:52 -0600

    log out doesn't use request params

--- src/main/java/com/example/sellthatthing/controllers/LoginController.java
+++ src/main/java/com/example/sellthatthing/controllers/LoginController.java
@@ -6,22 +6,38 @@ import org.springframework.security.web.WebAttributes;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.util.HashMap;
 
+/***
+ * @author Ife Sunmola
+ * Login Controller class
+ */
 @Controller
 @AllArgsConstructor
 @RequestMapping("/login")
+@SessionAttributes("message")
 public class LoginController {
     @GetMapping
     public String loadLoginPage(HttpServletRequest request) {
-        if (request.getUserPrincipal() == null) {
+        if (request.getUserPrincipal() == null) {// user is not logged in, show log in page
             return "login";
         }
-        return "redirect:/";
+        return "redirect:/";// user is logged in, show home page
+    }
+
+    @GetMapping("/logout")
+    public String logout(@ModelAttribute("message") HashMap<String, String> message) {
+        // on logout, redirect back to login page with a  success message
+        message.clear();
+        message.put("logoutSuccess", "You have been logged out");
+        return "redirect:login";
     }
 
     @GetMapping("/login-error")
