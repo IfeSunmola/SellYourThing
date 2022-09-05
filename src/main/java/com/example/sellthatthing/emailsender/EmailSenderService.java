3de592d93f4f5d37d9027d797f51e3891d5459aa
Ife <sunmolaife@gmail.com>
2022-09-05 00:26:38 -0600

    Added comments

--- src/main/java/com/example/sellthatthing/emailsender/EmailSenderService.java
+++ src/main/java/com/example/sellthatthing/emailsender/EmailSenderService.java
@@ -1,6 +1,8 @@
 package com.example.sellthatthing.emailsender;
 
 import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Async;
@@ -10,10 +12,17 @@ import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 import java.io.UnsupportedEncodingException;
 
+/**
+ * @author Ife Sunmola
+ * This class is used to send emails
+ */
 @Service
 @AllArgsConstructor
+@Async
 public class EmailSenderService {
     private final JavaMailSender mailSender;
+    private final Logger logger = LoggerFactory.getLogger(EmailSenderService.class);
+
 
     @Async
     public void sendMail(String subject, String fromEmail, String toEmail, String replyTo, String mailContent) {
@@ -28,8 +37,7 @@ public class EmailSenderService {
             mailSender.send(mimeMessage);
         }
         catch (MessagingException | UnsupportedEncodingException e) {
-            System.out.println("Email was not sent: " + e);
-            throw new IllegalStateException("failed to send email");
+            logger.warn("Email was not sent to " + toEmail + ". Exception message: " + e);
         }
     }
 }
