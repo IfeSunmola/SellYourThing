ddb1410d5f212d213e8e310e5d365855d8352950
Ife <sunmolaife@gmail.com>
2022-09-06 15:06:12 -0600

    UPDATED

--- src/main/java/com/example/sellthatthing/exceptions/ExceptionHandlerClass.java
+++ src/main/java/com/example/sellthatthing/exceptions/ExceptionHandlerClass.java
@@ -4,6 +4,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.multipart.MultipartException;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.HashMap;
 
@@ -23,17 +25,18 @@ public class ExceptionHandlerClass {
     }
 
     /**
+     * User tried uploading an image that's too big
+     */
+    @ExceptionHandler(MultipartException.class)
+    public String handleMultipartException(MultipartException ex) {
+        return "error-pages/405-error";
+    }
+
+    /**
      * Something was not found. Return 404 page
      */
     @ExceptionHandler(ResourceNotFoundException.class)
     public String resourceNotFound() {
         return "error-pages/404-error";
-    }
-
-    @ExceptionHandler
-    public ResponseEntity<HashMap<String, String>> emptyResource(EmptyResourceException ex) {
-        HashMap<String, String> errorMessage = new HashMap<>(1);
-        errorMessage.put("Error", ex.getMessage());
-        return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
     }
 }
